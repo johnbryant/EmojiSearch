d91653a014e2c45faf0da44b772b04872beb80c1
johnbryant <johnbryant262@gmail.com>
2019-06-30 12:15:22 -0400

    add clipboard

--- src/components/EmojiResult.js
+++ src/components/EmojiResult.js
@@ -1,14 +1,22 @@
 import React from "react";
 import EmojiResultItem from "./EmojiResultItem";
+import ClipBoard from "clipboard";
 
 class EmojiResult extends React.Component {
+  componentDidMount() {
+    this.clipboard = new ClipBoard(".copy-to-clipboard");
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
   render() {
     const emojis = this.props.emojis;
     return (
       <div>
-        {emojis.map(emoji => (
-          <EmojiResultItem emoji={emoji} />
-        ))}
+        {emojis.map(emoji => {
+          return <EmojiResultItem key={emoji.title} emoji={emoji} />;
+        })}
       </div>
     );
   }
--- src/components/EmojiResultItem.js
+++ src/components/EmojiResultItem.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "../styles/EmojiResultItem.css";
 
-const EmojiResultItem = ({ emoji }) => (
-  <div>
-    <span>{emoji.title}</span>
-  </div>
-);
+const EmojiResultItem = ({ emoji }) => {
+  const codePointHex = emoji.symbol.codePointAt(0).toString(16);
+  const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
+  return (
+    <div
+      className="emoji-result-item copy-to-clipboard"
+      data-clipboard-text={emoji.symbol}
+    >
+      <span className="left-container">
+        <img src={src} />
+        <span className="title">{emoji.title}</span>
+      </span>
+
+      <span className="info">Click to copy Emoji</span>
+    </div>
+  );
+};
 
 export default EmojiResultItem;
