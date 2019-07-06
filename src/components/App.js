aa2e1ccae626562c58396f24dc03687058727ad5
johnbryant <johnbryant262@gmail.com>
2019-07-06 13:59:23 -0400

    add paginator

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,63 @@ import React from "react";
 import Header from "./Header";
 import SearchInput from "./SearchInput";
 import EmojiResult from "./EmojiResult";
-import { emojiFilter } from "../controller/actions";
+import {
+  emojiFilter,
+  pageCounter,
+  emojiFilterPage
+} from "../controller/actions";
+import Paginate from "react-paginate";
+import "../styles/Paginate.css";
 
 class App extends React.Component {
   state = {
-    filter: emojiFilter("")
+    inputText: "",
+    filter: [],
+    page: []
   };
+
   inputChange = event => {
     this.setState({
-      filter: emojiFilter(event.target.value)
+      inputText: event.target.value,
+      filter: emojiFilter(event.target.value),
+      page: emojiFilterPage(event.target.value)
+    });
+    this.paginate.state.selected = 0;
+  };
+
+  handlePageClick = data => {
+    this.setState({
+      page: emojiFilterPage(this.state.inputText, data.selected)
     });
   };
 
+  componentDidMount() {
+    this.setState({
+      filter: emojiFilter(),
+      page: emojiFilterPage()
+    });
+  }
+
   render() {
+    const pageCount = pageCounter(this.state.filter);
+
     return (
       <div>
         <Header />
         <SearchInput onChange={this.inputChange} />
-        <EmojiResult emojis={this.state.filter} />
+        <EmojiResult emojis={this.state.page} />
+        <Paginate
+          ref={node => (this.paginate = node)}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={2}
+          previousLabel={"<<"}
+          nextLabel={">>"}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pagination page"}
+          activeClassName={"active"}
+        />
       </div>
     );
   }
--- src/controller/actions.js
+++ src/controller/actions.js
@@ -1,17 +1,26 @@
 import emojiList from "../data/emojiList.json";
 
-const emojiFilter = (searchText = "", maxShow = 30) => {
-  return emojiList
-    .filter(emoji => {
-      if (emoji.title.toLowerCase() === searchText.toLowerCase()) {
-        return true;
-      }
-      if (emoji.keywords.includes(searchText)) {
-        return true;
-      }
-      return false;
-    })
-    .slice(0, maxShow);
+const emojiFilter = (searchText = "", currentPage = 0, maxPerPage = 20) => {
+  return emojiList.filter(emoji => {
+    if (emoji.title.toLowerCase() === searchText.toLowerCase()) {
+      return true;
+    }
+    if (emoji.keywords.includes(searchText)) {
+      return true;
+    }
+    return false;
+  });
 };
 
-export { emojiFilter };
+const emojiFilterPage = (searchText = "", currentPage = 0, maxPerPage = 20) => {
+  return emojiFilter(searchText, currentPage, maxPerPage).slice(
+    currentPage * maxPerPage,
+    (currentPage + 1) * maxPerPage
+  );
+};
+
+const pageCounter = (list = emojiList, maxPerPage = 20) => {
+  return Math.floor(list.length / maxPerPage) + 1;
+};
+
+export { emojiFilter, emojiFilterPage, pageCounter };
